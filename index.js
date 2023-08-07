01452391ebba31d65ece72f84f89386096e64a67
kaan <kabuke13@gmail.com>
2023-08-07 16:04:31 +0100

    Lab Changes

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+const denverResults = [
+    {
+        year: 2013,
+        result: "L",
+    },
+    {
+        year: 2014,
+        result: "N/A",
+    },
+    {
+        year: 2015,
+        result: "W",
+    },
+]
+
+function superbowlWin(denverResults){
+    // let log = denverResults.find(function (item){
+    //     return item.result === "W"
+    // })
+    let log = denverResults.find(item => item.result === "W")
+    if(log === undefined){
+    return undefined
+   } else {
+    return log.year
+   }
+}
